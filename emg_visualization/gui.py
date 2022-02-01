aac900186b67dda2a41d4aa34cdc60ed8739a374
Petrus Kirsten <petrus.kirsten@usp.br>
2022-02-01 19:28:04 -0300

    update gui
    GUI exhibition improvements

--- emg_visualization/gui.py
+++ emg_visualization/gui.py
@@ -5,15 +5,15 @@ class Dialog(wx.Dialog):
     def __init__(self, parent):
         super().__init__(
             parent,
-            title="Automated motor mapping",
+            title='Automated motor mapping',
             style=wx.DEFAULT_DIALOG_STYLE | wx.FRAME_FLOAT_ON_PARENT,
         )
 
-        self.pathgenerated = False
+        self.noPath_traj = wx.StaticText(self, -1)
         self.initgui()
 
     def onpath(self, evt):
-        with wx.FileDialog(self, "Save EMG plots", wildcard="PNG files (*.png)|*.png",
+        with wx.FileDialog(self, 'Save EMG plots', wildcard='PNG files (*.png)|*.png',
                            style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT) as fileDialog:
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return
@@ -27,18 +27,21 @@ class Dialog(wx.Dialog):
             #     wx.LogError("Cannot save current data in file '%s'." % pathname)
 
     def oncancel(self, evt):
-        print("Closing...")
+        print('Closing...')
         self.Close(True)
 
     def onapply(self, evt):
-        print("Run realtime plot...")
+        print('Run realtime plot...')
 
     def onreset(self, evt):
-        print("Reseting path parameters values...")
+        print('Reseting path parameters values...')
 
     def ongenerate(self, evt):
-        print("Generating path...")
-        self.pathgenerated = True
+        print('Generating path...')
+        try:
+            self.noPath_traj.Destroy()
+        except RuntimeError:
+            pass
 
     def initgui(self):
         # EMG plotting GUI
@@ -86,49 +89,41 @@ class Dialog(wx.Dialog):
         self.Bind(wx.EVT_BUTTON, self.onapply, id=3)
 
         # Path generation GUI
-        txtctrl_x = wx.TextCtrl(self, -1, '0.00')
-        statxt_x = wx.StaticText(self, -1, 'X axis hotspot:')
-        txtctrl_y = wx.TextCtrl(self, -1, '0.00')
-        statxt_y = wx.StaticText(self, -1, 'Y axis hotspot:')
-        txtctrl_z = wx.TextCtrl(self, -1, '198.57')
-        statxt_z = wx.StaticText(self, -1, 'Z axis hotspot:')
+        ctrl_size = (60, -1)
+        x_ctrl = wx.TextCtrl(self, -1, '0.00', size=ctrl_size)
+        y_ctrl = wx.TextCtrl(self, -1, '0.00', size=ctrl_size)
+        z_ctrl = wx.TextCtrl(self, -1, '198.57', size=ctrl_size)
+        ecc_ctrl = wx.TextCtrl(self, -1, '0.75', size=ctrl_size)
+        radius_ctrl = wx.TextCtrl(self, -1, '40', size=ctrl_size)
+        pointsdist_ctrl = wx.TextCtrl(self, -1, '20', size=ctrl_size)
 
-        ecc_ctrl = wx.TextCtrl(self, -1, '0.75')
+        x_sta = wx.StaticText(self, -1, 'X axis hotspot:')
+        y_sta = wx.StaticText(self, -1, 'Y axis hotspot:')
+        z_sta = wx.StaticText(self, -1, 'Z axis hotspot:')
         ecc_sta = wx.StaticText(self, -1, 'Ellipse eccentricity:')
-        radius_ctrl = wx.TextCtrl(self, -1, '40')
         radius_sta = wx.StaticText(self, -1, 'Max ellipse radius [mm]:')
-        pointsdist_ctrl = wx.TextCtrl(self, -1, '20')
         pointsdist_sta = wx.StaticText(self, -1, 'Points distance [mm]:')
 
-        txt_traj = wx.FlexGridSizer(3, 4, 10, 10)
+        txt_traj = wx.FlexGridSizer(3, 4, 20, 15)
         txt_traj.AddMany(
-            ((statxt_x, 0, wx.ALIGN_CENTER_VERTICAL), (txtctrl_x, 0),
+            ((x_sta, 0, wx.ALIGN_CENTER_VERTICAL), (x_ctrl, 0),
              (ecc_sta, 0, wx.ALIGN_CENTER_VERTICAL), (ecc_ctrl, 0),
-             (statxt_y, 0, wx.ALIGN_CENTER_VERTICAL), (txtctrl_y, 0),
+             (y_sta, 0, wx.ALIGN_CENTER_VERTICAL), (y_ctrl, 0),
              (radius_sta, 0, wx.ALIGN_CENTER_VERTICAL), (radius_ctrl, 0),
-             (statxt_z, 0, wx.ALIGN_CENTER_VERTICAL), (txtctrl_z, 0),
+             (z_sta, 0, wx.ALIGN_CENTER_VERTICAL), (z_ctrl, 0),
              (pointsdist_sta, 0, wx.ALIGN_CENTER_VERTICAL), (pointsdist_ctrl, 0),)
         )
 
         buttons_traj = wx.BoxSizer(wx.HORIZONTAL)
-        if self.pathgenerated:
-            # path_traj = wx.StaticText(self, -1, '   Path generate successfully!   ')
-            path_traj = wx.StaticText(self, -1)
-            path_traj.SetLabel('   Path generate successfully!   ')
-            path_traj.SetBackgroundColour('GREEN')
-            buttons_traj.Add(
-                path_traj, 0,
-                wx.ALIGN_CENTER_VERTICAL
-            )
-        else:
-            # noPath_traj = wx.StaticText(self, -1, '   No path generate yet.   ')
-            noPath_traj = wx.StaticText(self, -1)
-            noPath_traj.SetLabel('   No path generate yet.   ')
-            noPath_traj.SetBackgroundColour('YELLOW')
-            buttons_traj.Add(
-                noPath_traj, 0,
-                wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, 0
-            )
+
+        # noPath_traj = wx.StaticText(self, -1, '   No path generate yet.   ')
+        self.noPath_traj.SetLabel('   No path generate yet.   ')
+        self.noPath_traj.SetBackgroundColour('YELLOW')
+        buttons_traj.Add(
+            self.noPath_traj, 0,
+            wx.ALIGN_CENTER_VERTICAL
+        )
+
         buttons_traj.Add(
             wx.Button(self, 4, 'Reset'),
             1, wx.ALIGN_CENTER_VERTICAL
@@ -145,7 +140,7 @@ class Dialog(wx.Dialog):
         )
         traj_sizer.Add(
             buttons_traj, 0,
-            wx.ALL, 10
+            wx.TOP, 23
         )
 
         self.Bind(wx.EVT_BUTTON, self.onreset, id=4)
